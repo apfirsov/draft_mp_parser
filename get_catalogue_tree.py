5743cbd4012111db35ac1c66c5a8f4e9d912414b
Александр Смирнов <5targazer@mail.ru>
2023-01-27 03:10:00 +0300

    added main catalogue parser

---
+++ get_catalogue_tree.py
@@ -0,0 +1,34 @@
+# скачивает основной многоуровневый каталог, убирает многоуровневость
+# и сохраняет в файл 'catalogue.json'
+
+import json
+import requests
+
+
+def get_catalogue_tree(dictionary):
+    for item in dictionary:
+        global RESULT
+        RESULT.append({
+            'id': item.get('id'),
+            'name': item.get('name'),
+            'parent': item.get('parent'),
+            'landing': item.get('landing'),
+            'seo': item.get('seo'),
+            'url': item.get('url'),
+            'shard': item.get('shard'),
+            'query': item.get('query')
+        })
+        item_childs = item.get('childs')
+        if item_childs is not None:
+            get_catalogue_tree(item_childs)
+
+
+if __name__ == '__main__':
+    RESULT = []
+    catalogue_url = 'https://static-basket-01.wb.ru/vol0/data/main-menu-ru-ru-v2.json'
+    response = requests.get(catalogue_url).json()
+
+    get_catalogue_tree(response)
+
+    with open('catalogue.json', 'w') as file:
+        json.dump(RESULT, file, indent="   ", ensure_ascii=False)
